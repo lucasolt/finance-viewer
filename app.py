6d4a0d2dae2aa19b64e83e698e67679e45fee6c3
rato <144276750+lucasolt@users.noreply.github.com>
2026-06-24 01:22:20 -0300

    Change bar color for 'Gastos' type in app.py

--- app.py
+++ app.py
@@ -677,7 +677,7 @@ with tab1:
                    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
                    use_container_width=True)
     else:
-        bar_color = "#cc2222" if tipo == "Gastos" else get_accent()
+        bar_color = "#cc1111" if tipo == "Gastos" else get_accent()
         por_mes=dff.groupby("mes")["valor_abs"].sum().reset_index().sort_values("mes")
         fig=go.Figure()
         fig.add_bar(x=por_mes["mes"],y=por_mes["valor_abs"],marker_color=bar_color,marker_line_width=0,hovertemplate="<b>%{x}</b><br>R$ %{y:,.2f}<extra></extra>")
